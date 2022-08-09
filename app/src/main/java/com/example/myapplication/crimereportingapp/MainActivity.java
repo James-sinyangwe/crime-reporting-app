0080d3695983a447262434ecfe163a7ff2498d88
james sinyangwe <Sinyangwejamie@gmail.com>
2022-08-09 16:38:12 +0200

    3rd commit,this contains firebase database connection

--- app/src/main/java/com/example/myapplication/crimereportingapp/MainActivity.java
+++ app/src/main/java/com/example/myapplication/crimereportingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.crimereportingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,20 +8,70 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://crime-reporting-app-6db86-default-rtdb.firebaseio.com/");
       private Button sign_up_button,login_button;
       private EditText username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username =(EditText) findViewById(R.id.user_name);
+        password =(EditText) findViewById(R.id.password);
         sign_up_button=(Button) findViewById(R.id.sign_up_button);
         sign_up_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent= new Intent(MainActivity.this,SignupActivity.class);
                 startActivity(intent);
+            }
+        });
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               final String usernameTxt =username.getText().toString();
+               final String passwordTXt =password.getText().toString();
+
+               if  (usernameTxt.isEmpty() || passwordTXt.isEmpty()){
+                   Toast.makeText(MainActivity.this,"Username or Password is Empty",Toast.LENGTH_SHORT).show();
+
+               }
+               else{
+                          databaseReference.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                              @Override
+                              public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                  // here will check if the user's phone number is in the database
+                                  if(snapshot.hasChild(usernameTxt)){
+
+                                      //if the number is in the database then we check if the phone number and password match
+                                      final String getpasswordTxt= snapshot.child(passwordTXt).child("password").getValue(String.class);
+
+                                      if(getpasswordTxt.equals(passwordTXt)){
+                                          Toast.makeText(MainActivity.this,"logged in Successfully",Toast.LENGTH_SHORT).show();
+
+                                      }else{
+                                          Toast.makeText(MainActivity.this,"Wrong password",Toast.LENGTH_SHORT).show();
+                                      }
+                                  }else{
+                                      Toast.makeText(MainActivity.this,"Wrong password",Toast.LENGTH_SHORT).show();
+                                  }
+                              }
+
+                              @Override
+                              public void onCancelled(@NonNull DatabaseError error) {
+
+                              }
+                          });
+               }
             }
         });
     }
--- app/src/main/java/com/example/myapplication/crimereportingapp/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/crimereportingapp/SignupActivity.java
@@ -1,13 +1,90 @@
 package com.example.myapplication.crimereportingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
-
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://crime-reporting-app-6db86-default-rtdb.firebaseio.com/");
+       private EditText firstname,lastname,NRCnumber,phonenumber,email,password1,password2;
+       private Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firstname =(EditText) findViewById(R.id.firstname);
+        lastname =(EditText) findViewById(R.id.lastname);
+        NRCnumber =(EditText) findViewById(R.id.nrcnumber);
+        phonenumber =(EditText) findViewById(R.id.phonenumber);
+        email =(EditText) findViewById(R.id.email);
+        password1 =(EditText) findViewById(R.id.password1);
+        password2 =(EditText) findViewById(R.id.password2);
+        submit =(Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String firstnameTxt = firstname.getText().toString();
+                final String lastnameTxt = lastname.getText().toString();
+                final String nrcTxt = NRCnumber.getText().toString();
+                final String phonenumberTxt = phonenumber.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String password1Txt = password1.getText().toString();
+                final String password2Txt = password2.getText().toString();
+
+                if(firstnameTxt.isEmpty() || lastnameTxt.isEmpty() || nrcTxt.isEmpty() || phonenumberTxt.isEmpty()
+                || emailTxt.isEmpty() || password1Txt.isEmpty() || password2Txt.isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Please fill all fields",Toast.LENGTH_SHORT).show();
+
+                }else if(!password1Txt.equals(password2Txt)){
+
+                    Toast.makeText(SignupActivity.this,"passwords are not matching",Toast.LENGTH_SHORT).show();
+
+
+                }else {
+                    databaseReference.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            // here we"re checking if the phone number is already registered
+                            if(snapshot.hasChild(phonenumberTxt)){
+                                Toast.makeText(SignupActivity.this,"phone number is already registered",Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                //database referencing
+                                databaseReference.child("users").child(phonenumberTxt).child("firstname").setValue(firstnameTxt);
+                                databaseReference.child("users").child(phonenumberTxt).child("lastname").setValue(lastnameTxt);
+                                databaseReference.child("users").child(phonenumberTxt).child("NRCNumber").setValue(nrcTxt);
+                                databaseReference.child("users").child(phonenumberTxt).child("email").setValue(emailTxt);
+                                databaseReference.child("users").child(phonenumberTxt).child("password1").setValue(password1Txt);
+                                databaseReference.child("users").child(phonenumberTxt).child("password2").setValue(password2Txt);
+
+                                Toast.makeText(SignupActivity.this,"User Registered Successfully",Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+
+
+            }
+        });
+
     }
 }
