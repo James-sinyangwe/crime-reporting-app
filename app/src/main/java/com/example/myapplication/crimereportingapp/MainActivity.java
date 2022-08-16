a15a2512515caa8276b35a2025730c18b6aea85b
james sinyangwe <Sinyangwejamie@gmail.com>
2022-08-16 12:38:21 +0200

    user registration and  login successful.

--- app/src/main/java/com/example/myapplication/crimereportingapp/MainActivity.java
+++ app/src/main/java/com/example/myapplication/crimereportingapp/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,14 +27,18 @@ public class MainActivity extends AppCompatActivity {
 
      DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://crime-reporting-app-6db86-default-rtdb.firebaseio.com/");
       private Button sign_up_button,login_button;
-      private EditText username,password;
+      private EditText email_Edittext,password_Edittext;
+      private FirebaseAuth mAuth;
+      ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username =(EditText) findViewById(R.id.user_name);
-        password =(EditText) findViewById(R.id.password);
+        email_Edittext=(EditText) findViewById(R.id.user_name);
+        password_Edittext =(EditText) findViewById(R.id.password);
         login_button =(Button) findViewById(R.id.login_button);
+        progressBar =(ProgressBar)findViewById(R.id.progressBar2);
+        mAuth=FirebaseAuth.getInstance();
         sign_up_button=(Button) findViewById(R.id.sign_up_button);
         sign_up_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,40 +50,41 @@ public class MainActivity extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               final String phonenumberTxt =username.getText().toString();
-               final String passwordTXt =password.getText().toString();
+               final String EmailTxt =email_Edittext.getText().toString();
+               final String passwordTXt =password_Edittext.getText().toString();
 
-               if  (phonenumberTxt.isEmpty() || passwordTXt.isEmpty()){
-                   Toast.makeText(MainActivity.this,"Username or Password is Empty",Toast.LENGTH_SHORT).show();
-
+               if  (EmailTxt.isEmpty()){
+                         email_Edittext.setError("Please Enter Email");
+                         email_Edittext.requestFocus();
+                         return;
                }
-               else{
-                          databaseReference.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
-                              @Override
-                              public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                  // here will check if the user's phone number is in the database
-                                  if(snapshot.hasChild(phonenumberTxt)){
+                if(!Patterns.EMAIL_ADDRESS.matcher(EmailTxt).matches()){
 
-                                      //if the number is in the database then we check if the phone number and password match
-                                      final String getpassword= snapshot.child(phonenumberTxt).child("password").getValue(String.class);
-
-                                      if(getpassword.equals(passwordTXt)){
-                                          Toast.makeText(MainActivity.this,"logged in Successfully",Toast.LENGTH_SHORT).show();
-
-                                      }else{
-                                          Toast.makeText(MainActivity.this,"Wrong password",Toast.LENGTH_SHORT).show();
-                                      }
-                                  }else{
-                                      Toast.makeText(MainActivity.this,"Wrong password",Toast.LENGTH_SHORT).show();
-                                  }
-                              }
-
-                              @Override
-                              public void onCancelled(@NonNull DatabaseError error) {
-
-                              }
-                          });
+                    email_Edittext.setError("Please Enter a Valid Email");
+                    email_Edittext.requestFocus();
+                    return;
+                }
+               if (passwordTXt.isEmpty()){
+                   password_Edittext.setError("Please Enter your password");
+                   password_Edittext.requestFocus();
+                           return;
                }
+               if(passwordTXt.length()<6){
+                   password_Edittext.setError("Minimum password length is 6 characters");
+               }
+               progressBar.setVisibility(View.VISIBLE);
+               mAuth.signInWithEmailAndPassword(EmailTxt,passwordTXt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                       if(task.isSuccessful()){
+                           Intent intent = new Intent(MainActivity.this,DashboardActivity.class);
+                           startActivity(intent);
+                   }else{
+                       Toast.makeText(MainActivity.this,"Faild to login! Please Check your Credentials!",Toast.LENGTH_LONG).show();
+                       }
+                   }
+               });
+
             }
         });
     }
